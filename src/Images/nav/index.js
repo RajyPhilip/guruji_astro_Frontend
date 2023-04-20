9a5e07882cea26ebabd8e63ae6d35703fcb490a6
Rajy <developerrajy@gmail.com>
2023-04-20 15:03:36 +0530

    navbar responsive

--- src/Images/nav/index.js
+++ src/Images/nav/index.js
@@ -5,8 +5,9 @@ import lockImg from './private.png';
 import verified from './vrified astrologer.png';
 import filledCircle from './nav_caraousel-eff.png';
 import unfilledCircle from './nav_caraousel_eff2.png';
+import securepay from './securePay.png';
 
 const navImages ={
-    profile,customerCare,refundImg,lockImg,verified,filledCircle,unfilledCircle
+    securepay,profile,customerCare,refundImg,lockImg,verified,filledCircle,unfilledCircle
 }
 export default navImages ;
--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -4,56 +4,82 @@ import React from 'react';
 import './navbar.css';
 import logo from '../../Images/gurujiLogo.png';
 import navImages from '../../Images/nav';
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 function Navbar() {
+ // background image
 
     return (
-        <div className='navbar' >
-            <div className='logo-container'>
-                <img src={logo} alt='logo' />
-                <p>Guruji</p>
-            </div>
-            <div className='navList'>
-                <span>Home</span>
-                <p>Call with Astrologer</p>
-                <p>Live(Coming Soon)  </p>
-            </div>
-            <div className='profile-container'>
-                <img src={navImages.profile} alt='logo' />
-            </div>
-
-            <div className='nav-center-title-container'>
-                <p>"Astrology for Clarity"</p>
-                <span>Your Name is a Vedic Astrologer and has expertise in Vaastu and Mantra Theraphy</span>
-            </div>
-            <div>
-                <button className='consult-btn'>Consult Now</button>
-            </div>
-
-            <div className='lower-nav' >
-                <img src={navImages.customerCare} alt='customer-care' />
-                <p>24 X 7 Customer Support</p>
-            </div>
-            <div className='refund' >
-                <img src={navImages.refundImg} alt='customer-care' />
-                <p>100% Refund if Unsatisfied</p>
-            </div>
-            <div className='refund privte' >
-                <img src={navImages.lockImg} alt='customer-care' />
-                <p>Private & Confidential</p>
-            </div>
-            <div className='refund verified' >
-                <img src={navImages.verified} alt='customer-care' />
-                <p>Verified &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Astrologer</p>
-            </div>
-            <div className='caraouse'>
-                <img src={navImages.filledCircle} />
-                <img src={navImages.unfilledCircle} />
-                <img src={navImages.unfilledCircle} />
-            </div>
+<div className="navbar" >
+    
+    <div className="navbar__navContainer">
+        <div className="navbar__logo">
+            <GiHamburgerMenu className="navbar__ham"/>
+            <img src={logo} alt="logo" />
+            <p>Guruji</p>
         </div>
+        <div className="navbar__nav">
+            <p>Home</p>
+            <p>Call with Astrologer</p>
+            <p>Live(Coming Soon) </p>
+        </div>
+        <div className="navbar__profile">
+            <img src={navImages.profile} alt="logo" />
+        </div>
+    </div>
+
+
+
+    <div className="navbar__title">
+        <p>"Astrology for Clarity"</p>
+        <article>
+            Your Name is a Vedic Astrologer and has expertise in Vaastu and Mantra
+            Theraphy
+        </article>
+    </div>
+
+    <div className="navbar__btnContainer">
+        <button className="navbar__consultBtn">Consult Now</button>
+    </div>
+
+    <div className="navbar__lowNav">
+        <div className="navbar__service">
+            <img src={navImages.customerCare} alt="customer-care" />
+            <p>24 X 7 Customer Support </p>
+        </div>
+        <div className="navbar__service ">
+            <img src={navImages.refundImg} alt="customer-care" className="refund" />
+            <p>100% Refund if Unsatisfied</p>
+        </div>
+        <div className="navbar__service">
+            <img src={navImages.lockImg} alt="customer-care" />
+            <p>Private & Confidential</p>
+        </div>
+        <div className="navbar__service">
+            <img src={navImages.verified} alt="customer-care" />
+            <p>
+            Verified <br /> Astrologer
+            </p>
+        </div>
+        <div className="navbar__service">
+            <img src={navImages.securepay} alt="customer-care" />
+            <p>
+            Secure <br /> Payment
+            </p>
+        </div>
+    </div>
+
+    <div className="navbar__cour">
+        <img src={navImages.bDot} />
+        <img src={navImages.lDot} />
+        <img src={navImages.lDot} />
+    </div>
+    </div>
     );
 
 }
 
 export default Navbar;
+
+
+
